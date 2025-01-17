0ed7eada8961417c2796cf49ff87630de8962e12
Ricardo Campeol <ricardo@equipe.ezoom.com.br>
2025-01-17 00:39:53 -0300

    feat: list and details page done

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -2,16 +2,49 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class Home
+class Home extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return new Response(
-            '<html><body>Hello World!</body></html>'
-        );
+        $response = $this->client->request('GET', 'https://api.pokemontcg.io/v2/cards', [
+            'query' => [
+                'pageSize' => '24'
+            ],
+        ]);
+        $cards = $response->toArray();
+
+        if ($response->getStatusCode() === 200) {
+            $cards = $cards['data'];
+        } else {
+            throw $this->createNotFoundException('There was an error fetching the cards.');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'cards' => $cards
+        ]);
+    }
+
+    #[Route('/details/{id}', name: 'details')]
+    public function details(string $id): Response
+    {
+        $response = $this->client->request('GET', "https://api.pokemontcg.io/v2/cards/$id");
+        $card = $response->toArray()['data'];
+
+        return $this->render('home/details.html.twig', [
+            'card' => $card,
+        ]);
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html.twig", // Incluir templates Twig
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
